e5a2720e1c84a023d2b0eda7b582b49e3b1d8eaf
jaylapse <52016738+jaylapse@users.noreply.github.com>
2025-05-17 17:51:14 -0400

    Delete gallery/Texas-2025/generate_gallery.py

--- gallery/Texas-2025/generate_gallery.py
+++
@@ -1,8 +0,0 @@
-import os
-
-files = [f for f in os.listdir('.') if f.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.webp'))]
-
-print('<div class="gallery">')
-for file in files:
-    print(f'  <img src="{file}" alt="">')
-print('</div>')
