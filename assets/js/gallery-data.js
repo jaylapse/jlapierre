2118152a3d8a2a92099eb0b22c9776a42171dfe2
JayLapse <52016738+jaylapse@users.noreply.github.com>
2025-06-20 18:37:30 -0400

    Gallery Overhaul

---
+++ assets/js/gallery-data.js
@@ -0,0 +1,44 @@
+const galleryConfigs = {
+  home: {
+    label: 'Home',
+    folder: 'Home/',
+    images: [
+      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
+      {src: 'photo.JPG', thumb: 'photo.JPG'}
+    ]
+  },
+  texas2025: {
+    label: 'Texas 2025',
+    folder: 'Texas 2025/',
+    images: [
+      {src: 'photo (10).JPG', thumb: 'photo (10).JPG'},
+      {src: 'photo (11).JPG', thumb: 'photo (11).JPG'},
+      {src: 'photo (12).JPG', thumb: 'photo (12).JPG'},
+      {src: 'photo (13).JPG', thumb: 'photo (13).JPG'},
+      {src: 'photo (14).JPG', thumb: 'photo (14).JPG'},
+      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
+      {src: 'photo (3).JPG', thumb: 'photo (3).JPG'},
+      {src: 'photo (5).JPG', thumb: 'photo (5).JPG'},
+      {src: 'photo (7).JPG', thumb: 'photo (7).JPG'},
+      {src: 'photo.JPG', thumb: 'photo.JPG'}
+    ]
+  },
+  thunderbayon: {
+    label: 'Thunder Bay ON',
+    folder: 'Thunder Bay ON/',
+    images: [
+      {src: 'photo (10).JPG', thumb: 'photo (10).JPG'},
+      {src: 'photo (11).JPG', thumb: 'photo (11).JPG'},
+      {src: 'photo (12).JPG', thumb: 'photo (12).JPG'},
+      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
+      {src: 'photo (3).JPG', thumb: 'photo (3).JPG'},
+      {src: 'photo (4).JPG', thumb: 'photo (4).JPG'},
+      {src: 'photo (5).JPG', thumb: 'photo (5).JPG'},
+      {src: 'photo (6).JPG', thumb: 'photo (6).JPG'},
+      {src: 'photo (7).JPG', thumb: 'photo (7).JPG'},
+      {src: 'photo (8).JPG', thumb: 'photo (8).JPG'},
+      {src: 'photo (9).JPG', thumb: 'photo (9).JPG'},
+      {src: 'photo.JPG', thumb: 'photo.JPG'}
+    ]
+  },
+};
