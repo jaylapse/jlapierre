f49c4a82416ed2ba84ea61b9a2dc1b1b174d2d39
JayLapse <52016738+jaylapse@users.noreply.github.com>
2025-06-20 23:52:29 -0400

    Gallery Changes

--- assets/js/gallery-data.js
+++ assets/js/gallery-data.js
@@ -1,44 +1,91 @@
 const galleryConfigs = {
+  animals: {
+    label: 'Animals',
+    folder: 'Animals/',
+    images: [
+      { type: "image", src: "birb.JPG", thumb: "birb.JPG" },
+      { type: "image", src: "bird.JPG", thumb: "bird.JPG" },
+      { type: "image", src: "birds feeding.JPG", thumb: "birds feeding.JPG" },
+      { type: "image", src: "dove 2.JPG", thumb: "dove 2.JPG" },
+      { type: "image", src: "dove.JPG", thumb: "dove.JPG" },
+      { type: "image", src: "IMG_00062.JPG", thumb: "IMG_00062.JPG" },
+      { type: "image", src: "IMG_00202.JPG", thumb: "IMG_00202.JPG" },
+      { type: "image", src: "IMG_00292.JPG", thumb: "IMG_00292.JPG" },
+      { type: "image", src: "see you.JPG", thumb: "see you.JPG" },
+      { type: "image", src: "squirrel.JPG", thumb: "squirrel.JPG" },
+      { type: "image", src: "winston.JPG", thumb: "winston.JPG" },
+      { type: "image", src: "woodpecker.JPG", thumb: "woodpecker.JPG" }
+    ]
+  },
   home: {
     label: 'Home',
     folder: 'Home/',
     images: [
-      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
-      {src: 'photo.JPG', thumb: 'photo.JPG'}
+      { type: "image", src: "20150627_142100.jpg", thumb: "20150627_142100.jpg" },
+      { type: "image", src: "20150627_210332.jpg", thumb: "20150627_210332.jpg" },
+      { type: "image", src: "photo (2).JPG", thumb: "photo (2).JPG" },
+      { type: "image", src: "photo.JPG", thumb: "photo.JPG" }
+    ]
+  },
+  misclocations: {
+    label: 'Misc Locations',
+    folder: 'Misc Locations/',
+    images: [
+      { type: "image", src: "2022_12_16 (705).jpg", thumb: "2022_12_16 (705).jpg" },
+      { type: "image", src: "2022_12_16 (713).jpg", thumb: "2022_12_16 (713).jpg" },
+      { type: "image", src: "2022_12_16 (717).jpg", thumb: "2022_12_16 (717).jpg" },
+      { type: "image", src: "2022_12_16 (740).jpg", thumb: "2022_12_16 (740).jpg" },
+      { type: "image", src: "DJI_0045.JPG", thumb: "DJI_0045.JPG" },
+      { type: "image", src: "DJI_0046.JPG", thumb: "DJI_0046.JPG" },
+      { type: "image", src: "DJI_0056.JPG", thumb: "DJI_0056.JPG" }
     ]
   },
   texas2025: {
     label: 'Texas 2025',
     folder: 'Texas 2025/',
     images: [
-      {src: 'photo (10).JPG', thumb: 'photo (10).JPG'},
-      {src: 'photo (11).JPG', thumb: 'photo (11).JPG'},
-      {src: 'photo (12).JPG', thumb: 'photo (12).JPG'},
-      {src: 'photo (13).JPG', thumb: 'photo (13).JPG'},
-      {src: 'photo (14).JPG', thumb: 'photo (14).JPG'},
-      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
-      {src: 'photo (3).JPG', thumb: 'photo (3).JPG'},
-      {src: 'photo (5).JPG', thumb: 'photo (5).JPG'},
-      {src: 'photo (7).JPG', thumb: 'photo (7).JPG'},
-      {src: 'photo.JPG', thumb: 'photo.JPG'}
+      { type: "image", src: "photo (10).JPG", thumb: "photo (10).JPG" },
+      { type: "image", src: "photo (11).JPG", thumb: "photo (11).JPG" },
+      { type: "image", src: "photo (12).JPG", thumb: "photo (12).JPG" },
+      { type: "image", src: "photo (13).JPG", thumb: "photo (13).JPG" },
+      { type: "image", src: "photo (14).JPG", thumb: "photo (14).JPG" },
+      { type: "image", src: "photo (2).JPG", thumb: "photo (2).JPG" },
+      { type: "image", src: "photo (3).JPG", thumb: "photo (3).JPG" },
+      { type: "image", src: "photo (5).JPG", thumb: "photo (5).JPG" },
+      { type: "image", src: "photo (7).JPG", thumb: "photo (7).JPG" },
+      { type: "image", src: "photo.JPG", thumb: "photo.JPG" }
     ]
   },
   thunderbayon: {
     label: 'Thunder Bay ON',
     folder: 'Thunder Bay ON/',
     images: [
-      {src: 'photo (10).JPG', thumb: 'photo (10).JPG'},
-      {src: 'photo (11).JPG', thumb: 'photo (11).JPG'},
-      {src: 'photo (12).JPG', thumb: 'photo (12).JPG'},
-      {src: 'photo (2).JPG', thumb: 'photo (2).JPG'},
-      {src: 'photo (3).JPG', thumb: 'photo (3).JPG'},
-      {src: 'photo (4).JPG', thumb: 'photo (4).JPG'},
-      {src: 'photo (5).JPG', thumb: 'photo (5).JPG'},
-      {src: 'photo (6).JPG', thumb: 'photo (6).JPG'},
-      {src: 'photo (7).JPG', thumb: 'photo (7).JPG'},
-      {src: 'photo (8).JPG', thumb: 'photo (8).JPG'},
-      {src: 'photo (9).JPG', thumb: 'photo (9).JPG'},
-      {src: 'photo.JPG', thumb: 'photo.JPG'}
+      { type: "image", src: "photo (10).JPG", thumb: "photo (10).JPG" },
+      { type: "image", src: "photo (11).JPG", thumb: "photo (11).JPG" },
+      { type: "image", src: "photo (12).JPG", thumb: "photo (12).JPG" },
+      { type: "image", src: "photo (2).JPG", thumb: "photo (2).JPG" },
+      { type: "image", src: "photo (3).JPG", thumb: "photo (3).JPG" },
+      { type: "image", src: "photo (4).JPG", thumb: "photo (4).JPG" },
+      { type: "image", src: "photo (5).JPG", thumb: "photo (5).JPG" },
+      { type: "image", src: "photo (6).JPG", thumb: "photo (6).JPG" },
+      { type: "image", src: "photo (7).JPG", thumb: "photo (7).JPG" },
+      { type: "image", src: "photo (8).JPG", thumb: "photo (8).JPG" },
+      { type: "image", src: "photo (9).JPG", thumb: "photo (9).JPG" },
+      { type: "image", src: "photo.JPG", thumb: "photo.JPG" }
+    ]
+  },
+  warped2025: {
+    label: 'Warped 2025',
+    folder: 'Warped 2025/',
+    images: [
+      { type: "image", src: "PXL_20250614_192849391.jpg", thumb: "PXL_20250614_192849391.jpg" },
+      { type: "image", src: "PXL_20250614_215442243.jpg", thumb: "PXL_20250614_215442243.jpg" },
+      { type: "image", src: "PXL_20250615_004125916.jpg", thumb: "PXL_20250615_004125916.jpg" },
+      { type: "image", src: "PXL_20250615_171002744.jpg", thumb: "PXL_20250615_171002744.jpg" },
+      { type: "image", src: "PXL_20250615_181627144.jpg", thumb: "PXL_20250615_181627144.jpg" },
+      { type: "image", src: "PXL_20250615_181632592.jpg", thumb: "PXL_20250615_181632592.jpg" },
+      { type: "image", src: "PXL_20250616_004836143.jpg", thumb: "PXL_20250616_004836143.jpg" },
+      { type: "image", src: "PXL_20250616_004837432.jpg", thumb: "PXL_20250616_004837432.jpg" }
     ]
   },
 };
